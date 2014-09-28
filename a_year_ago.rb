2cb7899395bf81bc82ff95f2168e1922a0a8b59e
taea <igiari@gmail.com>
2014-09-29 00:50:14 +0900

    rm unnecessary file

--- a_year_ago.rb
+++
@@ -1,9 +0,0 @@
-require 'uri'
-
-today = Time.now
-year = today.year - 1
-month = today.month
-day = today.day
-url = "http://pplog.net/my/posts?day=#{day}\&month=#{month}\&year=#{year}"
-uri = URI.parse(url.to_s)
-system %!xdg-open '#{uri}'!

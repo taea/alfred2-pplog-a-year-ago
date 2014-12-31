d510b0153424aefb2167e8ba69c4007702b14cd1
DQNEO <dqneoo@gmail.com>
2014-12-31 23:26:01 +0900

    no need to escape &

--- pplog_a_year_ago.rb
+++ pplog_a_year_ago.rb
@@ -2,5 +2,5 @@ today = Time.now
 year = today.year - 1
 month = today.month
 day = today.day
-url = "http://pplog.net/my/posts?day=#{day}\&month=#{month}\&year=#{year}"
+url = "http://pplog.net/my/posts?day=#{day}&month=#{month}&year=#{year}"
 system %!open '#{url}'!
